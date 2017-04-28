2a0a42f12c5b5bbc32de7edd0cd798733edcb8a9
zlby <zlby970129@163.com>
2017-04-28 16:44:38 +0800

    cnn-defining variables

--- CNN/cnn_mnist.py
+++ CNN/cnn_mnist.py
@@ -14,19 +14,21 @@ def compute_accuracy(v_xs, v_ys):
 
 
 def weight_variable(shape):
-    pass
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
 
 
 def bias_variable(shape):
-    pass
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 
-def conv2d(x, W):t
-    pass
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
-    pass
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
 
 xs = tf.placeholder(tf.float32, [None, 784])
 ys = tf.placeholder(tf.float32, [None, 10])
