413df409b4b3d8f38be87a6d774864e4977be881
zlby <zlby970129@163.com>
2017-04-28 22:24:30 +0800

    cnn_mnist will crash during processing, waiting to be solved

--- CNN/cnn_mnist.py
+++ CNN/cnn_mnist.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
@@ -9,7 +10,7 @@ def compute_accuracy(v_xs, v_ys):
     y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
     correct_prediction = tf.equal(tf.argmax(y_pre, 1), tf.argmax(v_ys, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    result = sess.run(accuracy, feed_dict={xs: v_xs. ys: v_ys, keep_prob: 1})
+    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
     return result
 
 
@@ -28,11 +29,43 @@ def conv2d(x, W):
 
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1])
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
 
 xs = tf.placeholder(tf.float32, [None, 784])
 ys = tf.placeholder(tf.float32, [None, 10])
 keep_prob = tf.placeholder(tf.float32)
+x_image = tf.reshape(xs, [-1, 28, 28, 1])
+
+
+# conv1 layer
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+
+# conv2 layer
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+
+# func1 layer
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+
+# func2 layer
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys *
                                               tf.log(prediction), reduction_indices=[1]))
@@ -47,3 +80,12 @@ with tf.Session() as sess:
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict={
                  xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
+        if i % 50 == 0:
+            # test_images = []
+            # test_lables = []
+            # for j in range(100):
+            #     randno = np.random.randint(0, 10000)
+            #     test_images.append(mnist.test.images[randno])
+            #     test_lables.append(mnist.test.labels[randno])
+            # print(compute_accuracy(test_images, test_lables))
+            print(compute_accuracy(mnist.test.images.next_batch(100), mnist.test.labels.next_batch(100)))
